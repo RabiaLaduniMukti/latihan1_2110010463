881f2539bf0a715eb96995afd4aa36f90476d6c8
RabiaLaduniMukti <rabialadunimukti10@gmail.com>
2023-06-12 20:59:42 +0800

    Membuat Class MatematikaBeraksi

--- src/hitung/matematikaberaksi.java
+++ src/hitung/matematikaberaksi.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author asus
- */
 public class matematikaberaksi {
+    public static void main (String[] args){
+        
+        //Membuat Objek
+        matematika rabia = new matematika (6,3);
+        
+        System.out.println("Penjumlahan :"+ rabia.setpenjumlahan());
+        System.out.println("Penjumlahan :"+ rabia.setpengurangan());
+        System.out.println("Penjumlahan :"+ rabia.setperkalian());
+        System.out.println("Penjumlahan :"+ rabia.setpembagian());
+        
     
+}
 }
