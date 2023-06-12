1a819816c8b4b773ed06d25568b82f616b8fd106
RabiaLaduniMukti <rabialadunimukti10@gmail.com>
2023-06-12 20:49:27 +0800

    Melengkapi Method Matematika

--- src/hitung/matematika.java
+++ src/hitung/matematika.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author asus
- */
 public class matematika {
+    //membuat atribut
+    double bil1,bil2;
     
+    //method constructor
+    public matematika (double bil1, double bil2){
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+        
+    }
+
+    //method accsessor
+    public double setpenjumlahan (){
+        return bil1 + bil2 ;
+    }
+    public double setpengurangan (){
+        return bil1 - bil2 ;
+}
+    public double setperkalian (){
+        return bil1 * bil2 ;
+}
+    public double setpembagian (){
+        return bil1 / bil2 ;
+    }
 }
